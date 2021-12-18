e58136f15e863b95624a50020f6ad15602e842e5
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-18 13:25:36 +0100

    commit

--- estudiantes_calificación.py
+++ estudiantes_calificación.py
@@ -4,19 +4,21 @@ import random
 import re
 import sys
 
-notas = [
-    ["73"],
-    ["67"],
-    ["38"],
-    ["33"]
-]
+notas1= ["73" , " "]
+notas2 = ["67" , " "]
+notas3 = ["38" , " "]
+notas4 = ["33" , " "]
 
-# if notas> 40:
-#     print("Aprobado")
-# else:
-#     print("Suspenso")
+if notas1> 40:
+    print("Aprobado")
+else:
+    print("Suspenso")
 
 i=0 
 while i * 5 < 100:
 	i = i + 1 
-	print(i * 5)
+	if i-notas1>=3:
+        print("La nota no se aproxima, la nota es:" , notas1)
+    else:
+        notas1= i
+        print("La nota se aproxima, tu nota es: " , notas1)
