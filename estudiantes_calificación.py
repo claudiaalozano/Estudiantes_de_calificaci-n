e405d1136acfcaf6e618f9323da7a90fdf73937c
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-18 13:46:51 +0100

    commit

--- estudiantes_calificación.py
+++ estudiantes_calificación.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 notas= [73 , 67 , 38 , 33]
-
+notas_finales=[]
 
 for i in range(len(notas)):
     if notas[i]> 40:
@@ -15,11 +15,17 @@ for i in range(len(notas)):
 
 
 for i in range (len(notas)):
-    i * 5 < 100
-    i= 0
-	i = i + 1 
-	if i-notas>=3:
-        print("La nota no se aproxima, la nota es:" , notas)
-    else:
-        notas1= i
-        print("La nota se aproxima, tu nota es: " , notas)
+    cociente= int(notas_finales/5+1)
+    multiplo= cociente * 5
+    
+    
+    
+    
+    #i * 5 < 100
+    #i= 0
+	#i = i + 1 
+	#if i-notas>=3:
+        #print("La nota no se aproxima, la nota es:" , notas)
+    #else:
+        #notas1= i
+        #print("La nota se aproxima, tu nota es: " , notas)
